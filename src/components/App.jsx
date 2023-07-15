391115f787e26f6ab1b99e1c0f0dce9e9253e2f9
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-07-15 15:14:22 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { toast } from 'react-toastify';
 import { ColorRing } from 'react-loader-spinner';
@@ -8,45 +8,40 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
 import css from './ImageGallery/ImageGallery.module.css';
 
-class App extends Component {
-  state = {
-    searchQuery: '',
-    data: [],
-    page: 1,
-    totalHits: null,
-    loading: false,
-    loader: false,
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [loader, setLoader] = useState(false);
+
+  const handleFormSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
   };
 
-  handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery });
-  };
-
-  async componentDidUpdate(prevProps, prevState) {
-    const prevSearchQuery = prevState.searchQuery;
-    const nextSearchQuery = this.state.searchQuery;
-
-    if (prevSearchQuery !== nextSearchQuery) {
-      await this.loadImages(nextSearchQuery, 1);
+  useEffect(() => {
+    if (!searchQuery) {
+      return;
     }
-  }
+    loadImages(searchQuery, 1);
+  }, [searchQuery]);
 
-  loadMoreImages = async () => {
-    const { searchQuery, page, totalHits, data } = this.state;
-
+  const loadMoreImages = async () => {
     if (data.length >= totalHits) {
       return;
     }
 
-    await this.loadImages(searchQuery, page + 1);
+    await loadImages(searchQuery, page + 1);
   };
 
-  loadImages = async (searchQuery, page) => {
+  const loadImages = async (searchQuery, page) => {
     try {
-      this.setState({ loading: true, loader: true });
+      setLoading(true);
+      setLoader(true);
 
-      const data = await getData(searchQuery, page);
-      const { hits, totalHits } = data;
+      const dataImages = await getData(searchQuery, page);
+      const { hits, totalHits } = dataImages;
 
       if (totalHits === 0) {
         toast.error('Oops! Enter a valid search query.');
@@ -54,59 +49,55 @@ class App extends Component {
       }
 
       if (page === 1) {
-        this.setState({ data: hits, totalHits });
+        setData([...hits]);
+        setTotalHits(totalHits);
       } else {
-        this.setState(prevState => ({
-          data: [...prevState.data, ...hits],
-        }));
+        setData(prevData => [...prevData, ...hits]);
       }
-      this.setState({ page });
+      setPage(page);
     } catch (error) {
       console.error(error);
       toast.error('Oops! Something went wrong. Try again later.');
     } finally {
-      this.setState({ loading: false, loader: false });
+      setLoading(false);
+      setLoader(false);
     }
   };
 
-  render() {
-    const { data, loader, loading, totalHits } = this.state;
-
-    return (
-      <div>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <div className={css.loaderWrapper}>
-          {loader && (
-            <ColorRing
-              visible={true}
-              height="80"
-              width="80"
-              ariaLabel="blocks-loading"
-              wrapperStyle={{}}
-              wrapperClass="blocks-wrapper"
-              colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-            />
-          )}
-        </div>
-        <ImageGallery data={data} />
-        {!loading && data.length > 0 && data.length < totalHits && (
-          <Button onClick={this.loadMoreImages} />
+  return (
+    <div>
+      <Searchbar onSubmit={handleFormSubmit} />
+      <div className={css.loaderWrapper}>
+        {loader && (
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+          />
         )}
-        <ToastContainer
-          position="top-right"
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
       </div>
-    );
-  }
-}
+      <ImageGallery data={data} />
+      {!loading && data.length > 0 && data.length < totalHits && (
+        <Button onClick={loadMoreImages} />
+      )}
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </div>
+  );
+};
 
 export default App;
